a7a2543c4437513d66a6c6a6401b74a0a602c2b2
Rick Newman <rick@richardnewman.dev>
2022-04-23 08:46:17 -0600

    adds vector to move platform throughout world.

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -1,21 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MovingPlatform.h"
 
 // Sets default values
 AMovingPlatform::AMovingPlatform()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -23,5 +20,6 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	PlatformVector.Y = PlatformVector.Y + 3;
+	SetActorLocation(PlatformVector);
 }
-
